81f8744106f409ea07e465aa6049e68ac9a9dcb7
LastNightmare00 <kiranthomas66@gmail.com>
2021-07-20 20:09:37 +0530

    Scan crashes after returning

--- lib/utils/scan.dart
+++ lib/utils/scan.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile_vision/flutter_mobile_vision.dart';
+import 'package:ocr_application/utils/routes.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({Key? key}) : super(key: key);
@@ -20,17 +21,17 @@ class _ScanPageState extends State<ScanPage> {
   Future<Null> _read() async {
     List<OcrText> words = [];
     try {
-      words = await FlutterMobileVision.read(
-        camera: OCR_CAM,
-        waitTap: true,
-      );
-
+      words = await FlutterMobileVision.read(camera: OCR_CAM, waitTap: false);
+      if (!mounted) return;
       setState(() {
         word = words[0].value;
       });
     } on Exception {
       words.add(OcrText('Unable to recognize the word'));
+      if (!mounted) return;
+      Navigator.pushNamed(context, MyRoutes.home);
     }
+    if (!mounted) return;
   }
 
   @override
